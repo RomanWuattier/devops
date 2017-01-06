f34e48c43779a4a4f541a7e37138c2f1806df32c
Flips Maxime <flips_m@epita.fr>
2017-01-06 21:26:17 +0100

    docker-compose done

--- RGeo/routes/index.js
+++ RGeo/routes/index.js
@@ -5,10 +5,10 @@ var path = require('path');
 var router = express.Router();
 
 var config = {
-    user: 'postgres', //env var: PGUSER
+    user: 'Roman', //env var: PGUSER
     database: 'geo', //env var: PGDATABASE
     password: '', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
+    host: 'db', // Server hosting the postgres database
     port: 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
